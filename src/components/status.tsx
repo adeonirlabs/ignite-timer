09ad4e982913a23a0c6875293aa483c5b9cd6e1e
Adeonir Kohl <adeonir@gmail.com>
2023-09-22 09:35:04 -0300

    create status component

---
+++ src/components/status.tsx
@@ -0,0 +1,22 @@
+import type { ComponentProps } from 'react'
+import type { VariantProps } from 'tailwind-variants'
+import { tv } from 'tailwind-variants'
+
+import { cn } from '~/utils/classnames'
+
+const status = tv({
+  base: 'flex items-center gap-2 before:h-2 before:w-2 before:rounded-full',
+  variants: {
+    variant: {
+      done: 'before:bg-green-500',
+      progress: 'before:bg-yellow-500',
+      failed: 'before:bg-red-500',
+    },
+  },
+})
+
+interface Props extends ComponentProps<'div'>, VariantProps<typeof status> {}
+
+export function Status({ className, variant, ...props }: Props) {
+  return <div className={cn(status({ variant, className }))} {...props} />
+}
