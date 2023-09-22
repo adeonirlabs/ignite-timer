3c75475fc547c474644f381fdb5d17504bfd593c
Adeonir Kohl <adeonir@gmail.com>
2023-09-21 22:27:47 -0300

    add styles to layout

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -4,9 +4,11 @@ import { Header } from '~/components/header'
 
 export function Layout() {
   return (
-    <main>
-      <Header />
-      <Outlet />
-    </main>
+    <div className="flex h-full items-center justify-center p-20">
+      <section className="mx-auto flex h-full w-full max-w-6xl flex-col rounded-lg bg-zinc-800 p-10">
+        <Header />
+        <Outlet />
+      </section>
+    </div>
   )
 }
