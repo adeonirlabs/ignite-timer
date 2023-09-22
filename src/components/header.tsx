3af97f0e8d87235bdd972382d9fd2834afb79faa
Adeonir Kohl <adeonir@gmail.com>
2023-09-21 23:11:25 -0300

    add logo and navigation to header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,27 @@
+import { ScrollText, Timer } from 'lucide-react'
+import { NavLink } from 'react-router-dom'
+
+import { Logo } from '~/components/logo'
+import { cn } from '~/utils/classnames'
+
 export function Header() {
-  return <h1>Header</h1>
+  const navLink = cn(
+    'flex h-12 w-12 items-center justify-center text-zinc-400 transition hover:text-blue-500',
+    'border-b-2 border-t-2 border-b-transparent border-t-transparent hover:border-b-blue-500',
+    'aria-[current=page]:text-blue-500',
+  )
+
+  return (
+    <header className="flex items-center justify-between">
+      <Logo className="h-10 w-10 text-blue-500" />
+      <nav className="flex items-center gap-4">
+        <NavLink to="/" className={navLink} title="Home">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" className={navLink} title="History">
+          <ScrollText size={24} />
+        </NavLink>
+      </nav>
+    </header>
+  )
 }
